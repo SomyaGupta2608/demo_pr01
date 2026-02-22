db1c0d35523b2490e54a487995bcae331fad719a
SomyaGupta2608 <somyag2608@gmail.com>
2026-02-22 13:07:41 +0530

    feature/UC4

---
+++ UC4.java
@@ -0,0 +1,10 @@
+public class UC4 {
+    public static void main(string[] args){
+        lines[0]=System.out.println(String.join(" ","   **   ","     **    "," ******   ",      *****  "));
+        lines[1]=System.out.println(String.join(" "," **  ** ","   **  **  "," **    ** ",     **      "));
+        lines[2]=System.out.println(String.join(" ","**    **","  **    ** "," **    ** ",    **       "));
+        lines[3]=System.out.println(String.join(" ","**    **","  **    ** "," ******   ",      ***    "));
+        lines[4]=System.out.println(String.join(" ","**    **","  **    ** "," **       ",         **  "));
+        lines[5]=System.out.println(String.join(" "," **  ** ","   **  **  "," **       ","       **   "));
+        lines[6]=System.out.println(String.join(" ","   **   ","     **    "," **       ","  ******    "));	
+		System.out.println(line);
